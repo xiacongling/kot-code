39971a8cc9a2b644e7b16eac791cceeebf9afbae
Congling XIA <xiacongling@xiaomi.com>
2021-11-02 08:20:19 +0800

    resolve 575 and 237

--- src/main/kotlin/me/xiacongling/code/leet/commons.kt
+++ src/main/kotlin/me/xiacongling/code/leet/commons.kt
@@ -21,7 +21,7 @@ class Node(var `val`: Int) {
     override fun toString(): String = `val`.toString()
 }
 
-class ListNode(val `val`: Int) {
+class ListNode(var `val`: Int) {
     var next: ListNode? = null
 
     override fun toString(): String {
@@ -33,6 +33,24 @@ class ListNode(val `val`: Int) {
         }
         str += "null"
         return str
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ListNode
+
+        if (`val` != other.`val`) return false
+        if (next != other.next) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = `val`
+        result = 31 * result + (next?.hashCode() ?: 0)
+        return result
     }
 
     companion object {
@@ -55,6 +73,8 @@ class ListNode(val `val`: Int) {
             return head
         }
     }
+
+
 }
 
 class TreeNode(var `val`: Int) {
--- src/main/kotlin/me/xiacongling/code/leet/k0/h2/s0230_0239.kt
+++ src/main/kotlin/me/xiacongling/code/leet/k0/h2/s0230_0239.kt
@@ -1,6 +1,7 @@
 package me.xiacongling.code.leet.k0.h2
 
 import me.xiacongling.code.leet.Difficulty
+import me.xiacongling.code.leet.ListNode
 import me.xiacongling.code.leet.Solution
 import me.xiacongling.code.leet.TreeNode
 
@@ -54,7 +55,91 @@ fun kthSmallest(root: TreeNode?, k: Int): Int {
     return found
 }
 
+@Solution(
+    id = 237, title = "", difficulty = Difficulty.EASY, description = """
+请编写一个函数，用于删除单链表中某个特定节点。在设计函数时需要注意，你无法访问链表的头节点head，
+只能直接访问要被删除的节点 。
+
+题目数据保证需要删除的节点不是末尾节点 。
+
+示例 1：
+
+4 -> 5 -> 1 -> 9
+     |    
+     v
+4 ------> 1 -> 9
+
+输入：head = [4,5,1,9], node = 5
+输出：[4,1,9]
+解释：指定链表中值为5的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9
+
+示例 2：
+
+4 -> 5 -> 1 -> 9
+          |    
+          v
+4 -> 5 ------> 9
+
+输入：head = [4,5,1,9], node = 1
+输出：[4,5,9]
+解释：指定链表中值为1的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9
+
+示例 3：
+
+1 -> 2 -> 3 -> 4
+          |
+          v
+1 -> 2 ------> 4
+
+输入：head = [1,2,3,4], node = 3
+输出：[1,2,4]
+
+示例 4：
+
+0 -> 1
+|
+v
+     1
+
+输入：head = [0,1], node = 0
+输出：[1]
+
+示例 5：
+
+-3 -> 5 -> -99
+ |
+ v
+      5 -> -99
+
+输入：head = [-3,5,-99], node = -3
+输出：[5,-99]
+
+提示：
+* 链表中节点的数目范围是 [2, 1000]
+* -1000 <= Node.val <= 1000
+* 链表中每个节点的值都是唯一的
+* 需要删除的节点 node 是链表中的一个有效节点，且不是末尾节点
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/delete-node-in-a-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+)
+fun deleteNode(node: ListNode?) {
+    var p = node
+    var pre: ListNode? = null
+    while (p?.next != null) {
+        p.`val` = p.next!!.`val`
+        pre = p
+        p = p.next
+    }
+    pre?.next = null
+}
+
 fun main() {
+    // 230
+    println("== Test Cases for #230 ==")
+
     val r1 = TreeNode(3)
     r1.left = TreeNode(1)
     r1.left!!.right = TreeNode(2)
@@ -72,4 +157,26 @@ fun main() {
     val r3 = TreeNode(1)
     r3.right = TreeNode(2)
     println(kthSmallest(r3, 2))
+
+    // 237
+    println("== Test Cases for #237 ==")
+    var input237 = ListNode.of(4, 5, 1, 9)
+    deleteNode(input237!!.next!!.next)
+    println(input237 == ListNode.of(4, 5, 9))
+
+    input237 = ListNode.of(4, 5, 1, 9)
+    deleteNode(input237!!.next)
+    println(input237 == ListNode.of(4, 1, 9))
+
+    input237 = ListNode.of(1, 2, 3, 4)
+    deleteNode(input237!!.next!!.next)
+    println(input237 == ListNode.of(1, 2, 4))
+
+    input237 = ListNode.of(0, 1)
+    deleteNode(input237)
+    println(input237 == ListNode(1))
+
+    input237 = ListNode.of(-3, 5, 99)
+    deleteNode(input237)
+    println(input237 == ListNode.of(5, 99))
 }
